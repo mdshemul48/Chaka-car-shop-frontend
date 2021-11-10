218a8aa4bcb8342123e743a80e2e9dbdbe077cb4
MD. Shimul <mdshemul480@gmail.com>
2021-11-10 13:43:04 +0600

    adding orders

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { AuthProvider } from './Contexts/AuthContext';
+
 import './App.css';
-
-
 import 'bootstrap/dist/css/bootstrap.min.css';
+import PrivateRoute from './Routes/PrivateRoute';
 function App() {
   return (
-    <div className='App'>
-      <h1>hello world</h1>
-    </div>
+    <AuthProvider>
+      <Router>
+        {/* <NavBar /> */}
+        <Switch>
+          <Route path='/' exact>
+            <h1>Home</h1>
+          </Route>
+          <Route path='/auth' exact>
+            <h1>Auth</h1>
+          </Route>
+          <PrivateRoute path='/orders'>
+            <h1>orders</h1>
+          </PrivateRoute>
+        </Switch>
+        {/* <Footer /> */}
+      </Router>
+    </AuthProvider>
   );
 }
 
